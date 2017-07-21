b0b7d993bcebf412eb5bed3013ff3e5265c503f9
rym garali <rym garali>
2017-07-21 10:45:51 +0000

    Ajoutez des fichiers projet.

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        { //arrange
+            var valuescontroller = new ValuesController();
+            //act
+            var result = valuescontroller.bmi(0, 0);
+
+            //assert 
+            Assert.AreEqual(0, result);
+
+        }
+    }
+}
---
+++ WebApplication4.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebApplication4;
+using WebApplication4.Controllers;
+
+namespace WebApplication4.Tests.Controllers
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            // Disposer
+            HomeController controller = new HomeController();
+
+            // Agir
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Affirmer
+            Assert.IsNotNull(result);
+            Assert.AreEqual("Home Page", result.ViewBag.Title);
+        }
+    }
+}
